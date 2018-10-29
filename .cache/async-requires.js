e6f8c44e14dee7ad2e4b72111752f63f1460c4c4
Taiyang <tai.huang@beetech.global>
2018-10-29 17:51:45 -0300

    fix(css): adjust background css

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,21 +2,21 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!D:\\Dev Me\\blacksun\\.cache\\dev-404-page.js"),
-  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!D:\\Dev Me\\blacksun\\src\\pages\\404.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!D:\\Dev Me\\blacksun\\src\\pages\\index.js"),
-  "component---src-pages-page-der-js": require("gatsby-module-loader?name=component---src-pages-page-der-js!D:\\Dev Me\\blacksun\\src\\pages\\page-der.js")
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/home/taiyang/Development/blacksun-site/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/home/taiyang/Development/blacksun-site/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/home/taiyang/Development/blacksun-site/src/pages/index.js"),
+  "component---src-pages-page-der-js": require("gatsby-module-loader?name=component---src-pages-page-der-js!/home/taiyang/Development/blacksun-site/src/pages/page-der.js")
 }
 
 exports.json = {
-  "layout-index.json": require("gatsby-module-loader?name=path---!D:\\Dev Me\\blacksun\\.cache\\json\\layout-index.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!D:\\Dev Me\\blacksun\\.cache\\json\\dev-404-page.json"),
-  "404.json": require("gatsby-module-loader?name=path---404!D:\\Dev Me\\blacksun\\.cache\\json\\404.json"),
-  "index.json": require("gatsby-module-loader?name=path---index!D:\\Dev Me\\blacksun\\.cache\\json\\index.json"),
-  "page-der.json": require("gatsby-module-loader?name=path---page-der!D:\\Dev Me\\blacksun\\.cache\\json\\page-der.json"),
-  "404-html.json": require("gatsby-module-loader?name=path---404-html!D:\\Dev Me\\blacksun\\.cache\\json\\404-html.json")
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/taiyang/Development/blacksun-site/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/home/taiyang/Development/blacksun-site/.cache/json/dev-404-page.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/home/taiyang/Development/blacksun-site/.cache/json/404.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/home/taiyang/Development/blacksun-site/.cache/json/index.json"),
+  "page-der.json": require("gatsby-module-loader?name=path---page-der!/home/taiyang/Development/blacksun-site/.cache/json/page-der.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/home/taiyang/Development/blacksun-site/.cache/json/404-html.json")
 }
 
 exports.layouts = {
-  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!D:/Dev Me/blacksun/.cache/layouts/index.js")
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/home/taiyang/Development/blacksun-site/.cache/layouts/index.js")
 }
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,21 +3,21 @@ const preferDefault = m => m && m.default || m
 
 
 exports.layouts = {
-  "layout---index": preferDefault(require("D:/Dev Me/blacksun/.cache/layouts/index.js"))
+  "layout---index": preferDefault(require("/home/taiyang/Development/blacksun-site/.cache/layouts/index.js"))
 }
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("D:\\Dev Me\\blacksun\\.cache\\dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("D:\\Dev Me\\blacksun\\src\\pages\\404.js")),
-  "component---src-pages-index-js": preferDefault(require("D:\\Dev Me\\blacksun\\src\\pages\\index.js")),
-  "component---src-pages-page-der-js": preferDefault(require("D:\\Dev Me\\blacksun\\src\\pages\\page-der.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/home/taiyang/Development/blacksun-site/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/taiyang/Development/blacksun-site/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/taiyang/Development/blacksun-site/src/pages/index.js")),
+  "component---src-pages-page-der-js": preferDefault(require("/home/taiyang/Development/blacksun-site/src/pages/page-der.js"))
 }
 
 exports.json = {
-  "layout-index.json": require("D:\\Dev Me\\blacksun\\.cache\\json\\layout-index.json"),
-  "dev-404-page.json": require("D:\\Dev Me\\blacksun\\.cache\\json\\dev-404-page.json"),
-  "404.json": require("D:\\Dev Me\\blacksun\\.cache\\json\\404.json"),
-  "index.json": require("D:\\Dev Me\\blacksun\\.cache\\json\\index.json"),
-  "page-der.json": require("D:\\Dev Me\\blacksun\\.cache\\json\\page-der.json"),
-  "404-html.json": require("D:\\Dev Me\\blacksun\\.cache\\json\\404-html.json")
+  "layout-index.json": require("/home/taiyang/Development/blacksun-site/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/home/taiyang/Development/blacksun-site/.cache/json/dev-404-page.json"),
+  "404.json": require("/home/taiyang/Development/blacksun-site/.cache/json/404.json"),
+  "index.json": require("/home/taiyang/Development/blacksun-site/.cache/json/index.json"),
+  "page-der.json": require("/home/taiyang/Development/blacksun-site/.cache/json/page-der.json"),
+  "404-html.json": require("/home/taiyang/Development/blacksun-site/.cache/json/404-html.json")
 }
