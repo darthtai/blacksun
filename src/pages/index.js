0801859c3a98ad0c48b92bf8707840c0a1e41eb5
Taiyang <tai.huang@beetech.global>
2018-12-14 16:25:51 -0200

    fix(layout): adjust css and swap frame for project to better layout design
    Adjust css to remove repeated code and swap frame component to project component
    to make layout easier to look at and more consistent.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Frame, Content, Words, Footer, createTheme, Arwes, ThemeProvider, Link } from 'arwes';
+import { Header, Project, Content, Words, Footer, createTheme, Arwes, ThemeProvider, Link, Row } from 'arwes';
 
 import { homeFrame } from '../components/organism/homeFrame';
 import pattern from '../../resources/images/glow.png';
@@ -10,23 +10,27 @@ const IndexPage = () => (
   <ThemeProvider theme={theme}>
     <Arwes animate show pattern={pattern}>
       <Header animate show>
-        <div className='headerText'>
+        <div className='u-text-center'>
           <h1>
             <span>Welcome to the Black Sun</span>
           </h1>
         </div>
       </Header>
-      <div className='mainContainer'>
-        <Frame animate level={3} corners={4}>
+      <div className='mainContainer u-text-center'>
+        <Project
+          animate 
+          header='Welcome to my front end playground'
+          headerSize='h3'
+        >
           <Content className='mainContent'>
-            <Words animate>The site is still under construction while I learn more about Gatsby and React :)</Words>
+            <Words animate>The site is being worked on while I learn more about Gatsby and React</Words>
           </Content>
-        </Frame>
+        </Project>
       </div>
-        <Footer animlate className='footer'>
-          <Words animate>Check out the code</Words>
-          <Link href='https://github.com/darthtai/blacksun' target='_blank'> here</Link>
-        </Footer>
+      <Footer animate className='u-text-center'>
+        <Words animate>Check out the code</Words>
+        <Link href='https://github.com/darthtai/blacksun' target='_blank'> here</Link>
+      </Footer>
     </Arwes>
   </ThemeProvider>
 );
