e9aadad8211ed49e616895692e977ee9b90d966c
Taiyang <tai000@msn.com>
2019-01-05 13:16:04 -0200

    chore: remove cache folder

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/taiyang/Development/blacksun-site/src/layouts/index.js"
-  import data from "/home/taiyang/Development/blacksun-site/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
