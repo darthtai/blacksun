fc160adb2723d15c9b8ca74f2c5dcb0ffb21d049
Taiyang <tai000@msn.com>
2019-01-05 13:12:50 -0200

    feat(SoundProvider): implement sound provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,49 @@
 import React from 'react';
-import { Header, Project, Content, Words, Footer, createTheme, Arwes, ThemeProvider, Link, Row } from 'arwes';
+import { Header, Project, Content, Words, Footer, createTheme, Arwes, ThemeProvider, Link, Row, SoundsProvider, createSounds, withSounds, Button } from 'arwes';
 
 import { homeFrame } from '../components/organism/homeFrame';
 import pattern from '../../resources/images/glow.png';
+import test from '../../resources/sounds/test.mp3';
 
 const theme = createTheme();
 
+const mySounds = {
+  shared: { volume: 1, },  // Shared sound settings
+  players: {  // The player settings
+    deploy: {  // With the name the player is created
+      sound: { src: [test] }  // The settings to pass to Howler
+    }
+  }
+};
+
 const IndexPage = () => (
   <ThemeProvider theme={theme}>
-    <Arwes animate show pattern={pattern}>
-      <Header animate show>
-        <div className='u-text-center'>
-          <h1>
-            <span>Welcome to the Black Sun</span>
-          </h1>
+    <SoundsProvider sounds={createSounds(mySounds)}>
+      <Arwes animate show pattern={pattern}>
+        <Header animate show>
+          <div className='u-text-center'>
+            <h1>
+              <span>Welcome to the Black Sun</span>
+            </h1>
+          </div>
+        </Header>
+        <div className='mainContainer u-text-center'>
+          <Project
+            animate
+            header='Welcome to my front end playground'
+            headerSize='h3'
+          >
+            <Content className='mainContent'>
+              <Words animate>The site is being worked on while I learn more about Gatsby and React</Words>
+            </Content>
+          </Project>
         </div>
-      </Header>
-      <div className='mainContainer u-text-center'>
-        <Project
-          animate 
-          header='Welcome to my front end playground'
-          headerSize='h3'
-        >
-          <Content className='mainContent'>
-            <Words animate>The site is being worked on while I learn more about Gatsby and React</Words>
-          </Content>
-        </Project>
-      </div>
-      <Footer animate className='u-text-center'>
-        <Words animate>Check out the code</Words>
-        <Link href='https://github.com/darthtai/blacksun' target='_blank'> here</Link>
-      </Footer>
-    </Arwes>
+        <Footer animate className='u-text-center'>
+          <Words animate>Check out the code</Words>
+          <Link href='https://github.com/darthtai/blacksun' target='_blank'> here</Link>
+        </Footer>
+      </Arwes>
+    </SoundsProvider>
   </ThemeProvider>
 );
 
